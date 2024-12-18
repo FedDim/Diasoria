f50b81a409a5d34aa12bb964b3d32e334b391907
FedDim <146709037+FedDim@users.noreply.github.com>
2024-12-18 14:05:58 +0300

    Add Frame and Fix BIG problem
    The name in Title was changed to Diasoria, and a BIG problem was also solved with the fact that the folder where the pages were located was named Page, which caused a conflict that broke the project
    For the future, before committing and branch, launch the project, even if there were very few changes

---
+++ Pages/CreationLoadProjectPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Diasoria.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для CreationLoadProjectPage.xaml
+    /// </summary>
+    public partial class CreationLoadProjectPage : Page
+    {
+        public CreationLoadProjectPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Pages/ProjectEditorPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Diasoria.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ProjectEditorPage.xaml
+    /// </summary>
+    public partial class ProjectEditorPage : Page
+    {
+        public ProjectEditorPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
